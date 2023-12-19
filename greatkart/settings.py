270c7804d95f335314dd279bcc64481536c764f8
sudee <sudeep200393@gmail.com>
2023-12-19 11:37:12 +0530

    django authenticate

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -137,4 +137,20 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    
+}
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sudeep200393@gmail.com'
+EMAIL_HOST_PASSWORD = 'qgrtuezuvmtkojvi'
+
