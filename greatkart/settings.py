87b79bbc80b99a2e35b268650001a177d4379589
sudee <sudeep200393@gmail.com>
2023-12-23 16:48:31 +0530

    Product

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
-    'carts'
+    'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -125,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    'greatkart/static',
-]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- store/models.py
+++ store/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 from category.models import Category
 from django.urls import reverse
-
+from accounts.models import Account
+from django.db.models import Avg
 
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
@@ -22,6 +23,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+    
+    def averageReview(self):
+        reviews=ReviewRating.objects.filter(product=self,status=True).aggregate(average=Avg('rating'))
+        avg=0
+        if reviews['average'] is not None:
+            avg=float(reviews['average'])
+        return avg 
 
 
 class VariationManager(models.Manager):
@@ -50,3 +58,19 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+    
+class ReviewRating(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user=models.ForeignKey(Account,on_delete=models.CASCADE)
+    subject=models.CharField(max_length=100,blank=True)
+    review=models.TextField(max_length=500,blank=True)
+    rating=models.FloatField()
+    ip=models.CharField(max_length=20,blank=True)
+    status=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.subject
+    
+
